c3eef09ab23dc4566b03e73338249cd3926ef2d1
Ronald Moore <ronaldmoore30@gmail.com>
2024-11-04 20:17:16 -0600

    creating the attack button

--- script.js
+++ script.js
@@ -3,18 +3,28 @@
 // Example:
 // let fungusHP = 100;
 
-function onReady() {
-    console.log("Ready to go!")
+
+function attackButtonHandler(attackBtn){
     
+    console.log('attack button clicked', attackButtonHandler)
+
+    let attackBtn = document.querySelector(".attack-btn arcane-sceptor")
+   // attackBtn = document.getElementsByClassName("enemy") = dragon.hp = dragon.hp - 10
+   // let fungusHp = document.getElementsByClassName("enemy").getElementsById("hp-meter")
+    fungusHp -- 
+    console.log('arcane sceptor attack')
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
     
     // 🧠 Remember
     // - Handle events that ->
+    // use the attack button 
+    // drops the hp-meter value
+
     // - Updates state which is ->
     // - Rendered to the DOM
+    render ()
 }
 
 
-onReady()
