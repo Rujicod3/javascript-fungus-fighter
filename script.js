f9d3b2dd4dc010e1301706f21a3e60ad5ae6c3b2
Ronald Moore <ronaldmoore30@gmail.com>
2024-11-04 21:41:16 -0600

    added conditionals for attacks

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
-// let fungusHP = 100;
+ let fungusHP = 100;
+ let ap = 100;
 
+function attackButtonHandler(event){
+    let clickedButton = event.target
 
-function attackButtonHandler(attackBtn){
+    if (clickedButton.className.includes("arcane-scepter")) {
+        console.log('Arcane-scepter attack used', attackButtonHandler)
+        ap -= 12
+        fungusHP -= 14
     
-    console.log('attack button clicked', attackButtonHandler)
+    } else if (clickedButton.className.includes("entangle")) {   
+        console.log('Entangle attack used', attackButtonHandler)
+        ap -= 29
+        fungusHP -= 9
+    } else if (clickedButton.className.includes("dragon-blade")) {
+        console.log('Dragon-Blade attack used', attackButtonHandler)
+        ap -= 38
+        fungusHP -= 47
 
-    let attackBtn = document.querySelector(".attack-btn arcane-sceptor")
-   // attackBtn = document.getElementsByClassName("enemy") = dragon.hp = dragon.hp - 10
-   // let fungusHp = document.getElementsByClassName("enemy").getElementsById("hp-meter")
-    fungusHp -- 
+    } else if (clickedButton.className.includes("star-fire")) {
+        console.log('Star-fire attack used', attackButtonHandler)
+
+        ap -= 33
+        fungusHP -= 25
+    
+    } else {
+        console.log("WTF???")
+        return;
+    }
+    
+    if (ap < 0) {
+        ap = 0
+        //disable buttons
+    }
+
+    if (fungusHP < 0) {
+        fungusHP = 0
+    }
+   
+    let  = document.querySelector(".attack-btn arcane-sceptor")
+   
     console.log('arcane sceptor attack')
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
-    
     // 🧠 Remember
     // - Handle events that ->
-    // use the attack button 
-    // drops the hp-meter value
-
     // - Updates state which is ->
     // - Rendered to the DOM
     render ()
